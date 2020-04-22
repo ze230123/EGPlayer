2d42de64dc98ca4b584e5ede87fe429200e25e30
张泽群 <zhangzh@eagersoft.cn>
2020-04-22 17:52:50 +0800

    init

---
+++ EGPlayer/EGPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EGPlayer
+//
+//  Created by youzy01 on 2020/4/22.
+//  Copyright © 2020 youzy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
