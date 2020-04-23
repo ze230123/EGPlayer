c8e0baf3a5a9715da7f644af95ab7a345afd649c
张泽群 <zhangzh@eagersoft.cn>
2020-04-23 14:39:21 +0800

    添加KVO

--- EGPlayer/EGPlayer/EGPlayer/EGPlayer.swift
+++ EGPlayer/EGPlayer/EGPlayer/EGPlayer.swift
@@ -13,6 +13,7 @@ class EGPlayer {
     let player = AVPlayer()
 
     var displayView: DisplayerLayer!
+    var controlView: (UIView & PlayerControlable)!
 
     init<V: UIView>(controlView: V) where V: PlayerControlable {
         displayView = DisplayerLayer()
@@ -50,13 +51,17 @@ class EGPlayer {
     var timeObserverToken: Any?
 
     var loadedTimeRangesObser: NSKeyValueObservation?
+    var playbackBufferEmptyObser: NSKeyValueObservation?
+    var playbackLikelyToKeepUpObser: NSKeyValueObservation?
+
+    var timeControlStatusObser: NSKeyValueObservation?
 }
 
 extension EGPlayer {
     func addKVO(item: AVPlayerItem) {
         /// item状态
         statusObser = item.observe(\.status, changeHandler: { (item, _) in
-            print(item.status.rawValue)
+            print(item.status.rawValue, item.error?.localizedDescription)
         })
         /// 缓冲
         loadedTimeRangesObser = item.observe(\.loadedTimeRanges, changeHandler: { (item, _) in
@@ -67,10 +72,21 @@ extension EGPlayer {
             let total = item.duration.seconds
             print("缓冲", cache, total, cache / total)
         })
+
+        playbackBufferEmptyObser = item.observe(\.isPlaybackBufferEmpty) { (item, _) in
+            print("缓存不够了 自动暂停播放")
+        }
+
+        playbackLikelyToKeepUpObser = item.observe(\.isPlaybackLikelyToKeepUp, changeHandler: { (item, _) in
+            print("缓存好了 手动播放")
+        })
     }
 
     func removeKVO() {
         statusObser = nil
+        loadedTimeRangesObser = nil
+        playbackBufferEmptyObser = nil
+        playbackLikelyToKeepUpObser = nil
     }
 }
 
@@ -86,12 +102,36 @@ extension EGPlayer {
 
     func prepare() {
         displayView.setPlayer(player)
+        addPlayerNotification()
+
         /// 播放时间
         let interval = CMTime(seconds: 1, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
         timeObserverToken = player.addPeriodicTimeObserver(forInterval: interval, queue: .main) { [unowned self] (time) in
             let currentTime = time.seconds
             let totalTime = self.player.currentItem?.duration.seconds ?? 0
+            self.controlView.setPlayTime(currentTime, total: totalTime)
             print(currentTime, totalTime, Float(currentTime / totalTime))
         }
+
+        timeControlStatusObser = player.observe(\.timeControlStatus) { [unowned self] (item, _) in
+            self.controlView.playStatueDidChanged()
+        }
+    }
+
+    func addPlayerNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(playFinish), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(playError), name: Notification.Name.AVPlayerItemFailedToPlayToEndTime, object: nil)
+    }
+
+    @objc func playFinish() {
+        print("播放完成")
+        player.seek(to: CMTime(seconds: 0, preferredTimescale: 1), toleranceBefore: .zero, toleranceAfter: .zero)
+    }
+
+    @objc func playError() {
+        guard let error = player.error else { return }
+        controlView.playFaild(error: error)
+        print("播放失败", error.localizedDescription)
+        player.status
     }
 }
--- EGPlayer/EGPlayer/EGPlayer/EGPlayerControlView.swift
+++ EGPlayer/EGPlayer/EGPlayer/EGPlayerControlView.swift
@@ -9,30 +9,54 @@
 import UIKit
 import AVFoundation
 
-class EGPlayerControlView: UIView, PlayerControlable {
+class EGPlayerControlView: UIView, NibLoadable, PlayerControlable {
+    func playFaild(error: Error) {
+    }
+    
+    func playDidCache() {
+    }
+    
+    func playDidCanPlay() {
+    }
+    
+    func setCacheProgress(_ progress: Double) {
+    }
+
+    func setPlayTime(_ time: Double, total: Double) {
+    }
+
+    func playStatueDidChanged() {
+    }
 
     @IBOutlet weak var topView: UIView!
     @IBOutlet weak var backButton: UIButton!
-    
+
     @IBOutlet weak var bottomView: UIView!
     @IBOutlet weak var playerButton: UIButton!
-    
+
     @IBOutlet weak var currentTimeLabel: UILabel!
     @IBOutlet weak var showButton: UIButton!
     @IBOutlet weak var allTimeLabel: UILabel!
     @IBOutlet weak var sliderView: UIView!
 
-weak var player: AVPlayer?
+    weak var player: AVPlayer?
 
-    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initViewFromNib()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initViewFromNib()
+    }
+
     static var nib: UINib? {
         return UINib(nibName: String(describing: self), bundle: nil)
     }
 
     override func awakeFromNib() {
         super.awakeFromNib()
- 
     }
-
 }
     
