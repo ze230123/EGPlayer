6c131f56540b1814c6c3621b894075a757894c23
张泽群 <zhangzh@eagersoft.cn>
2020-04-23 18:13:42 +0800

    旋转测试

--- EGPlayer/EGPlayer/Example/PlayerViewController.swift
+++ EGPlayer/EGPlayer/Example/PlayerViewController.swift
@@ -23,6 +23,8 @@ class PlayerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.isHidden = true
+
         slider.setThumbImage(UIColor.red.asImage(CGSize(width: 14, height: 14)), for: .normal)
 
         let url = "http://tb-video.bdstatic.com/tieba-smallvideo-transcode/3612804_e50cb68f52adb3c4c3f6135c0edcc7b0_3.mp4"
@@ -37,29 +39,41 @@ class PlayerViewController: UIViewController {
         player.pause()
     }
 
+    @IBAction func fullAction(_ sender: UIButton) {
+        if sender.isSelected {
+            setIntface(.portrait)
+        } else {
+            setIntface(.landscapeRight)
+        }
+
+        sender.isSelected = !sender.isSelected
+    }
+
+    // TODO: 屏幕旋转：1、手动将设备旋转，2、监听系统通知当发生旋转时自动更新布局(2暂时不需要)
+    func setIntface(_ orientation: UIInterfaceOrientation) {
+        UIView.animate(withDuration: 0.5) {
+            UIDevice.current.setValue(orientation.rawValue, forKey: "orientation")
+            self.view.setNeedsLayout()
+        }
+    }
+
     @IBAction func valueChanged(_ sender: UISlider) {
         print(sender.value)
     }
 }
-
-
+// MARK: - 删除
 extension UIColor {
-        func asImage(_ size: CGSize) -> UIImage? {
-            
-            var resultImage: UIImage? = nil
-            let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-            UIGraphicsBeginImageContextWithOptions(rect.size, false, UIScreen.main.scale)
-            
-            guard let context = UIGraphicsGetCurrentContext() else {
-                
-                return resultImage
-            }
-            
-            context.setFillColor(self.cgColor)
-            context.fill(rect)
-            resultImage = UIGraphicsGetImageFromCurrentImageContext()
-            UIGraphicsEndImageContext()
-            
+    func asImage(_ size: CGSize) -> UIImage? {
+        var resultImage: UIImage? = nil
+        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, UIScreen.main.scale)
+        guard let context = UIGraphicsGetCurrentContext() else {
             return resultImage
         }
+        context.setFillColor(self.cgColor)
+        context.fill(rect)
+        resultImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return resultImage
+    }
 }
