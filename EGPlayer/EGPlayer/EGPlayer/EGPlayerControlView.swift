11f6894627a52a53526ba770dbc634f414f2abda
liyulong <liyl@eagersoft.cn>
2020-04-23 11:55:03 +0800

    添加竖屏xib

--- EGPlayer/EGPlayer/EGPlayer/EGPlayerControlView.swift
+++ EGPlayer/EGPlayer/EGPlayer/EGPlayerControlView.swift
@@ -9,4 +9,25 @@
 import UIKit
 
 class EGPlayerControlView: UIView {
+    
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var backButton: UIButton!
+    
+    @IBOutlet weak var bottomView: UIView!
+    @IBOutlet weak var playerButton: UIButton!
+    
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var showButton: UIButton!
+    @IBOutlet weak var allTimeLabel: UILabel!
+    @IBOutlet weak var sliderView: UIView!
+    
+    static var nib: UINib? {
+        return UINib(nibName: String(describing: self), bundle: nil)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+ 
+    }
+    
 }
