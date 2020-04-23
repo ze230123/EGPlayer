f4ed33fed02767071874f761ca3b735039acb33a
张泽群 <zhangzh@eagersoft.cn>
2020-04-23 17:42:39 +0800

    slider定义样式

--- EGPlayer/EGPlayer/Example/PlayerViewController.swift
+++ EGPlayer/EGPlayer/Example/PlayerViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class PlayerViewController: UIViewController {
 
     @IBOutlet weak var displayView: DisplayerLayer!
+    @IBOutlet weak var slider: UISlider!
 
     lazy var controlView = EGPlayerControlView()
 
@@ -22,6 +23,8 @@ class PlayerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        slider.setThumbImage(UIColor.red.asImage(CGSize(width: 14, height: 14)), for: .normal)
+
         let url = "http://tb-video.bdstatic.com/tieba-smallvideo-transcode/3612804_e50cb68f52adb3c4c3f6135c0edcc7b0_3.mp4"
         player.setUrl(url)
     }
@@ -35,6 +38,28 @@ class PlayerViewController: UIViewController {
     }
 
     @IBAction func valueChanged(_ sender: UISlider) {
-        player.player.volume = sender.value
+        print(sender.value)
     }
+}
+
+
+extension UIColor {
+        func asImage(_ size: CGSize) -> UIImage? {
+            
+            var resultImage: UIImage? = nil
+            let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+            UIGraphicsBeginImageContextWithOptions(rect.size, false, UIScreen.main.scale)
+            
+            guard let context = UIGraphicsGetCurrentContext() else {
+                
+                return resultImage
+            }
+            
+            context.setFillColor(self.cgColor)
+            context.fill(rect)
+            resultImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            
+            return resultImage
+        }
 }
