cdb2d8f337ddd8ced377d9abc163c96c9a42c29b
张泽群 <zhangzh@eagersoft.cn>
2020-04-24 11:26:05 +0800

    旋转测试

---
+++ EGPlayer/EGPlayer/EGPlayer/EGOrientationObserver.swift
@@ -0,0 +1,26 @@
+//
+//  EGOrientationObserver.swift
+//  EGPlayer
+//
+//  Created by youzy01 on 2020/4/24.
+//  Copyright © 2020 youzy. All rights reserved.
+//
+
+import Foundation
+
+/*
+ 1. 点击按钮全屏：点击按钮 -> player -> observer -> player -> view
+ 2. 系统通知: 旋转 -> 
+ 
+ */
+
+
+class EGOrientationObserver {
+
+    
+    
+    /// 添加观察者
+    func addOrientationObserver() {}
+    /// 删除观察者
+    func removeOrientationObserver() {}
+}
